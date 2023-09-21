e466129049841e84860f4f26273fab930374ded7
Neelesh1907 <119998532+Neelesh1907@users.noreply.github.com>
2023-09-21 13:04:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 //your JS code here. If required.
-const button = document.getElementById("enterBtn");
-button.addEventListener("click",()=>{
-	const heading = document.createElement("h1");
-	heading.id = "status";
-	heading.innerText = `Entered Metaverse`;
-	const referenceNode = document.getElementById("status");
-	const body = document.getElementById("status").parentNode;
-	body.replaceChild(heading,referenceNode);
+// const button = document.getElementById("enterBtn");
+// button.addEventListener("click",()=>{
+// 	const heading = document.createElement("h1");
+// 	heading.id = "status";
+// 	heading.innerText = `Entered Metaverse`;
+// 	const referenceNode = document.getElementById("status");
+// 	const body = document.getElementById("status").parentNode;
+// 	body.replaceChild(heading,referenceNode);
+// });
+
+let status = document.getElementById('status');
+let enterBtn = document.getElementById('enterBtn');
+enterBtn.addEventListener('click', () => {
+	status.innerHTML = "<h1>Entered Metaverse</h1>";
 });
