189aa907a2b776694a58a92c73c915abf02c2f4b
Neelesh1907 <119998532+Neelesh1907@users.noreply.github.com>
2023-09-21 13:01:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const button = document.getElementById("enterBtn");
+button.addEventListener("click",()=>{
+	const heading = document.createElement("h1");
+	heading.id = "status";
+	heading.innerText = `Entered Metaverse`;
+	const referenceNode = document.getElementById("status");
+	const body = document.getElementById("status").parentNode;
+	body.replaceChild(heading,referenceNode);
+});
